479f101954bd43b896a5a5754743932f1dec2400
Diego Pandolfo <pandolfo@gmail.com>
2019-10-18 11:24:21 -0300

    lab

--- puppet/modules/puppet-web/manifests/init.pp
+++ puppet/modules/puppet-web/manifests/init.pp
@@ -2,6 +2,11 @@ class puppet-web {
 
 package { "apache2": ensure => installed }
 
+package { "php5": ensure => installed }
+package { "libapache2-mod-php5": ensure => installed }
+package { "php5-mcrypt": ensure => installed }
+package { "php5-mysql": ensure => installed }
+
 ->
 
 service { "apache2": ensure => "running" }
